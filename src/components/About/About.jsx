81a61bd62a6536e091b2bc35c94c0f3a2209622a
catplotlib <43911345+Puja2402@users.noreply.github.com>
2021-07-13 16:20:10 +0530

    facicon

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -27,12 +27,12 @@ const About = () => {
       <Container>
         <Title title="About Me" />
         <Row className="about-wrapper">
-          <Col md={6} sm={12}>
-            <Fade bottom duration={1000} delay={600} distance="30px">
+          <Col md={3} sm={12}>
+            {/* <Fade bottom duration={1000} delay={600} distance="30px">
               <div className="about-wrapper__image">
                 <AboutImg alt="profile picture" filename={img} />
               </div>
-            </Fade>
+            </Fade> */}
           </Col>
           <Col md={6} sm={12}>
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
