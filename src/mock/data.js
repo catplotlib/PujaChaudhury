2d6f52e6a5afe89fc7438ce3cdb89e4bb08960ab
catplotlib <43911345+Puja2402@users.noreply.github.com>
2021-07-22 15:19:13 +0530

    Added app

--- src/mock/data.js
+++ src/mock/data.js
@@ -22,8 +22,8 @@ export const aboutData = {
     'My name is Puja Chaudhury. I am a Front End Engineer currently in Bangalore. I would like to think of myself as a motivated developer who loves to code and create beautiful and functional websites.',
   paragraphTwo:
     'I am very fond of the Developer community and I try to learn new things in the devspace every day. I believe and contribute to open source as it helps me grow as a developer and I like to build in public as well. Currently I am attempting the #100daysOCode Challenge. You can track my progress on Twitter! ',
-  paragraphThree: 'Frontend Skills: React, Javascript, jQuery',
-  paragraphFour: 'Backend Skills: Nodejs, Expressjs',
+  paragraphThree: 'Frontend Skills: React, React Native, Javascript, jQuery, Gatsby',
+  paragraphFour: 'Backend Skills: Nodejs, Expressjs, MongoDB',
   resume: 'https://drive.google.com/file/d/1oziHS6qCXek92z27u-kN9mLBN-nWLBJQ/view?usp=sharing', // if no resume, the button will not show up
 };
 
@@ -32,7 +32,7 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project6.png',
-    title: 'Aximos-The Adventure Game',
+    title: 'Aximos-The Adventure Game (React)',
     info: 'This is a choose your own story, text based game. Made completely in React and deployed using netlify. I used useState to set up the states of the players choices and carried them forward in their next choices.',
     info2: '',
     url: 'https://text-based-adventure-game.netlify.app/',
@@ -40,8 +40,17 @@ export const projectsData = [
   },
   {
     id: nanoid(),
+    img: 'project7.png',
+    title: 'The Movie Quiz (React-Native)',
+    info: 'A simple app with minimal UI for a quiz application on movies.',
+    info2: '',
+    url: 'https://expo.dev/@catplotlib/moviequiz',
+    repo: 'https://github.com/catplotlib/MovieQuiz', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
     img: 'project1.png',
-    title: 'Stardew Valley',
+    title: 'Stardew Valley (React)',
     info: 'A mini-project developed from scratch showcasing information about the RPG game-Stardew Valley. Made completely in React and styled in CSS.',
     info2: '',
     url: 'https://stardew-valley.netlify.app/',
@@ -50,16 +59,17 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project2.png',
-    title: 'BarTinder',
+    title: 'BarTinder (ejs)',
     info: 'A completely responsive and dynamic Full stack Express.js Webapp that recommends cocktails based on filters!',
     info2: '',
     url: 'https://bar-tinder.herokuapp.com/',
     repo: 'https://github.com/catplotlib/Bar-Tinder', // if no repo, the button will not show up
   },
+
   {
     id: nanoid(),
     img: 'project3.png',
-    title: 'Product Catalogue',
+    title: 'Product Catalogue (React)',
     info: 'A demo webapp to display and filter products based on their categories.',
     info2: '',
     url: 'https://my-shein-catalogue.netlify.app/',
@@ -68,21 +78,21 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project4.png',
-    title: 'Notebook styled Shopping List',
+    title: 'Notebook styled Shopping List (React)',
     info: 'A notebook-style shopping list app with edit and delete features, made in react.',
     info2: '',
     url: 'https://our-shopping-list.netlify.app/',
     repo: 'https://github.com/catplotlib/ReactShoppingList', // if no repo, the button will not show up
   },
-  {
-    id: nanoid(),
-    img: 'project5.png',
-    title: 'Text Generator',
-    info: 'A placeholder text generator where the user can set the number of paragraphs they need. Handled the edge cases as well! Used a sort of stitching effect as my CSS.',
-    info2: '',
-    url: 'https://lorem-ipsum-replacer.netlify.app/',
-    repo: 'https://github.com/catplotlib/ReactText-Generator', // if no repo, the button will not show up
-  },
+  // {
+  //   id: nanoid(),
+  //   img: 'project5.png',
+  //   title: 'Text Generator',
+  //   info: 'A placeholder text generator where the user can set the number of paragraphs they need. Handled the edge cases as well! Used a sort of stitching effect as my CSS.',
+  //   info2: '',
+  //   url: 'https://lorem-ipsum-replacer.netlify.app/',
+  //   repo: 'https://github.com/catplotlib/ReactText-Generator', // if no repo, the button will not show up
+  // },
 ];
 
 // CONTACT DATA
